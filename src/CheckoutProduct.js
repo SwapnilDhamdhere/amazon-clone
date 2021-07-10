7e848fc591025c14551675062972ad7236474890
Swapnil Dhamdhere <swapnil.s.dhamdhere@gmail.com>
2021-07-10 21:01:45 +0530

    now

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./CheckoutProduct.css";
 import StarIcon from "@material-ui/icons/Star";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  const removeFromBasket = () => {
+    //remove the item from the basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} />
@@ -19,8 +29,8 @@ function CheckoutProduct({ id, image, title, price, rating }) {
               <StarIcon />;
             })}
         </div>
-        <button>Remove from Basket</button>
-        {/* 2:50:23 */}
+        <button onClick={removeFromBasket}>Remove from Basket</button>
+        {/*3:22:20 */}
       </div>
     </div>
   );
